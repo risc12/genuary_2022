baeaffd00bcc11c6c50832110b0eaa3b11fbf6a3
Remco Peggeman <r.peggeman@youngcapital.nl>
2022-01-06 19:18:16 +0100

    Day two and three

---
+++ 1/1.js
@@ -0,0 +1,34 @@
+import { Canvas, Random } from '/toolkit/index.js';
+import noise from './../toolkit/noise.js';
+
+console.log('Hello!')
+
+function first() {
+  noise.seed(Math.random());
+  const ctx = Canvas.create2D('', 800, 800);
+
+  const W = ctx.canvas.width;
+  const H = ctx.canvas.height;
+
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);  
+
+
+  for(let i = 0; i < 10_000; i ++) {
+    const location = [
+      Random.normal() * W,
+      Random.normal() * H,
+    ];
+
+    const size =  Random.normal(1,5);
+
+    const hl = i > 9990;
+
+    const gs = (noise.perlin2(location[0], location[1]) + 0.5) / 2;
+
+    const color = hl ? 'rgba(0, 255, 255, 1)' : `rgba(${gs * 255}, ${gs * 255}, ${gs * 50}, ${gs})`;
+
+    Canvas.drawCircle(ctx, location, size, { stroke: color, strokeWidth: 1 });
+  }
+}
+
+first();
